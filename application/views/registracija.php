8677894f636c79dc2dfd9ea9527d18f8da23a1df
Master <evgen.tusek@student.um.si>
2018-09-13 00:12:09 +0200

    sa

--- application/views/registracija.php
+++ application/views/registracija.php
@@ -19,215 +19,243 @@
 		<link href='https://fonts.googleapis.com/css?family=Passion+One' rel='stylesheet' type='text/css'>
 		<link href='https://fonts.googleapis.com/css?family=Oxygen' rel='stylesheet' type='text/css'>
 
+
 <style>
+body {}
 
-body{background-image: url("https://cdn.zuerich.com/sites/default/files/styles/sharing/public/web_zuerich_home_topevents_1600x900.jpg?itok=yjC-dXXH");
-	 background-repeat:no-repeat;
-	 background-size:cover;
-	 width:100%;
-	 height:100vh;
-	 overflow:auto;
-	 
+body {
+  background-image: url("https://cdn.zuerich.com/sites/default/files/styles/sharing/public/web_zuerich_home_topevents_1600x900.jpg?itok=yjC-dXXH");
+  
+  background-size: 2000px 1000px;
+	
+ <!-- font: 400 14px 'Calibri','Arial';-->
+  padding: 20px;
+  margin:5%;padding:5;
 }
 
-/*-----for border----*/
-.container{ 
-	font-family:Roboto,sans-serif;
-	  background-image:url(https://image.freepik.com/free-vector/dark-blue-blurred-background_1034-589.jpg) ;
-    
-     border-style: 1px solid grey;
-     margin: 0 auto;
-     text-align: center;
-     opacity: 0.8;
-     margin-top: 67px;
-   box-shadow: 2px 5px 5px 0px #eee;
-     max-width: 500px;
-     padding-top: 10px;
-     height: 363px;
-     margin-top: 166px;
+.GumbObDogodkih {
+	background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #d6d6d6), color-stop(1, #ffffff));
+	background:-moz-linear-gradient(top, #d6d6d6 5%, #ffffff 100%);
+	background:-webkit-linear-gradient(top, #d6d6d6 5%, #ffffff 100%);
+	background:-o-linear-gradient(top, #d6d6d6 5%, #ffffff 100%);
+	background:-ms-linear-gradient(top, #d6d6d6 5%, #ffffff 100%);
+	background:linear-gradient(to bottom, #d6d6d6 5%, #ffffff 100%);
+	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#d6d6d6', endColorstr='#ffffff',GradientType=0);
+	background-color:#d6d6d6;
+	-moz-border-radius:13px;
+	-webkit-border-radius:13px;
+	border-radius:13px;
+	border:1px solid #6e6e6e;
+	display:inline-block;
+	cursor:pointer;
+	color:#000000;
+	font-family:Arial;
+	font-size:17px;
+	font-weight:bold;
+	padding:7px 13px;
+	text-decoration:none;
 }
-/*--- for label of first and last name---*/
-.lastname{
-	 margin-left: 1px;
-     font-family: sans-serif;
-     font-size: 14px;
-     color: white;
-     margin-top: 10px;
+.GumbObDogodkih:hover {
+	background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #ffffff), color-stop(1, #d6d6d6));
+	background:-moz-linear-gradient(top, #ffffff 5%, #d6d6d6 100%);
+	background:-webkit-linear-gradient(top, #ffffff 5%, #d6d6d6 100%);
+	background:-o-linear-gradient(top, #ffffff 5%, #d6d6d6 100%);
+	background:-ms-linear-gradient(top, #ffffff 5%, #d6d6d6 100%);
+	background:linear-gradient(to bottom, #ffffff 5%, #d6d6d6 100%);
+	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#ffffff', endColorstr='#d6d6d6',GradientType=0);
+	background-color:#ffffff;
 }
-.firstname{
-	 margin-left: 1px;
-     font-family: sans-serif;
-     font-size: 14px;
-     color: white;
-     margin-top: 5px;
+.GumbObDogodkih:active {
+	position:relative;
+	top:1px;
 }
-#lname{
-	 margin-top:5px;
+
+.gumbDodajDogodek {
+	-moz-box-shadow:inset 0px 1px 0px 0px #ffffff;
+	-webkit-box-shadow:inset 0px 1px 0px 0px #ffffff;
+	box-shadow:inset 0px 1px 0px 0px #ffffff;
+	background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #f9f9f9), color-stop(1, #e9e9e9));
+	background:-moz-linear-gradient(top, #f9f9f9 5%, #e9e9e9 100%);
+	background:-webkit-linear-gradient(top, #f9f9f9 5%, #e9e9e9 100%);
+	background:-o-linear-gradient(top, #f9f9f9 5%, #e9e9e9 100%);
+	background:-ms-linear-gradient(top, #f9f9f9 5%, #e9e9e9 100%);
+	background:linear-gradient(to bottom, #f9f9f9 5%, #e9e9e9 100%);
+	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#f9f9f9', endColorstr='#e9e9e9',GradientType=0);
+	background-color:#f9f9f9;
+	-moz-border-radius:6px;
+	-webkit-border-radius:6px;
+	border-radius:6px;
+	border:1px solid #dcdcdc;
+	display:inline-block;
+	cursor:pointer;
+	color:#666666;
+	font-family:Arial;
+	font-size:15px;
+	font-weight:bold;
+	padding:6px 24px;
+	text-decoration:none;
+	text-shadow:0px 1px 0px #ffffff;
 }
+
+  .col-1 {width: 14.28%;}
+.col-2 {width: 16.66%;}
+.col-3 {width: 25%;}
+.col-4 {width: 33.33%;}
+.col-5 {width: 41.66%;}
+.col-6 {width: 50%;}
+.col-7 {width: 58.33%;}
+.col-8 {width: 66.66%;}
+.col-9 {width: 65%;}
+.col-10 {width: 83.33%;}
+.col-11 {width: 90%;}
+.col-12 {width: 100%;}
+  
+table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+}
+
+td, th {
+    border: 1px solid #dddddd;
+    text-align: center;
+    padding: 8px;
+}
+th
+{
+background-color:black;
+color:white;
+}
+th:first-child, td:first-child
+{
+  position:sticky;
+  left:0px;
+ 
+}
+ td:first-child
+ {
+  background-color:grey;
+ }
+ .vSredino{
+    border: 1px ;
+    text-align: center;
+    padding: 8px;
+}
+
+input, textarea{
+width:100%;
+}
+
+textarea{
+resize: vertical;
+} 
+h1 {
+    text-align: center;
+    width: 100%;
+	font-size: 500%;
+	<!--font-family: 'Holtwood One SC';-->
+	text-color:blue;
+    padding: 10px;
+}
+#glava{
 	  
-
-/*---for heading-----*/
-.heading{
-	 text-decoration:bold;
-	 text-align : center;
-	 font-size:30px;
-	 color:#F96;
-	 padding-top:10px;
+	  color:white; 
+	  backdrop-filter: blur(5px);
+	  font-family: 'Times New Roman', Times, serif;
+  }
+  .rcorners1 {
+    border: 5px solid #FBEEC1;
+    border-radius: 1em;
 }
-/*-------for email----------*/
-  /*------label----*/
-#email{
-	  margin-top: 5px;
-}
-/*-----------for Password--------*/
-     /*-------label-----*/
-.mail{
-	 margin-left: 44px;
-     font-family: sans-serif;
-     color: white;
-     font-size: 14px;
-     margin-top: 13px;
-}
-.pass{
-	 color: white;
-     margin-top: 9px;
-     font-size: 14px;
-     font-family: sans-serif;
-     margin-left: 6px;
-     margin-top: 9px;
-}
-#password{
- margin-top: 6px;
-}
-/*------------for phone Number--------*/
-      /*----------label--------*/
-.pno{
-	 font-size: 18px;
-     margin-left: -13px;
-     margin-top: 10px;
-     color: #ff9;
-	
-}	
-
-/*--------------for Gender---------------*/
-     /*--------------label---------*/
-.gender {
-	 color: white;
-     font-family: sans-serif;
-     font-size: 14px;
-     margin-left: 28px;
-     margin-top: 8px;
-}
-
-     /*---------- for Input type--------*/
-.col-xs-4.male{
-	 color: white;
-     font-size: 13px;
-     margin-top: 9px;
-     padding-bottom: 16px;
-}
-.col-xs-4.female {
-     color: white;
-     font-size: 13px;
-     margin-top: 9px;
-     padding-bottom: 16px;
-	 padding-right: 95px;
-}	
-/*------------For submit button---------*/
-.sbutton{
-	 color: white;
-     font-size: 20px;
-     border: 1px solid white;
-     background-color: #080808;
-     width: 32%;
-     margin-left: 41%;
-     margin-top: 16px;
-	 box-shadow: 0px 2px 2px 0px white;
-  	   
-   }
-.btn.btn-warning:hover {
-    box-shadow: 2px 1px 2px 3px #99ccff;
-	background:#5900a6;
-	color:#fff;
-	transition: background-color 1.15s ease-in-out,border-color 1.15s ease-in-out,box-shadow 1.15s ease-in-out;
-	
-}	 
+  
 
 </style>
 </head>
-<body> 
- <div class="container">
- <!---heading---->
-     <header class="heading"> Registracija</header><hr></hr>
-	<!---Form starting----> 
-	<div class="row ">
-	 <!--- For Name---->
-         <div class="col-sm-12">
-             <div class="row">
-			     <div class="col-xs-4">
-          	         <label class="firstname">Ime :</label> </div>
-		         <div class="col-xs-8">
-		           <input placeholder="Vpišite ime" type="text" class="form-control" name="ime" id="ime"  required/>
-             </div>
-		      </div>
-		 </div>
-		 
-		 
-         <div class="col-sm-12">
-		     <div class="row">
-			     <div class="col-xs-4">
-                     <label class="lastname">Priimek :</label></div>
-				<div class ="col-xs-8">	 
-			<input placeholder="Vpišite priimek" type="text" name="priimek" id="priimek" class="form-control" required>
-                </div>
-		     </div>
-		 </div>
-     <!-----For email---->
-		 <div class="col-sm-12">
-		     <div class="row">
-			     <div class="col-xs-4">
-		             <label class="mail">Email :</label></div>
-			     <div class="col-xs-8"	>	 
-								<input placeholder="Vpišite vaš email račun" class="form-control" type="text" name="email" id="email" required>
-		         </div>
-		     </div>
-		 </div>
-		 <div class="col-sm-12">
-		     <div class="row">
-			     <div class="col-xs-4">
-		             <label class="mail" >Potrditev Email-a :</label></div>
-			     <div class="col-xs-8"	>	 
-			<input placeholder="Vpišite še enkrat vaš e-mail" type="text"  class="form-control" name="emailPonovno" id="emailPonovno" required>
-		         </div>
-		     </div>
-		 </div>
-	 <!-----For Password and confirm password---->
-          <div class="col-sm-12">
-		         <div class="row">
-				     <div class="col-xs-4">
-		 	              <label class="pass">Potrditev gesla :</label></div>
-				  <div class="col-xs-8">
-			<input placeholder="Vpišite geslo" type="password" name="geslo" id="geslo" class="form-control" required>
-				 </div>
-          </div>
-		  </div>
-		  
-          <div class="col-sm-12">
-		         <div class="row">
-				     <div class="col-xs-4">
-		 	              <label class="pass">potrditev gesla :</label></div>
-				  <div class="col-xs-8">
-			<input placeholder="Vpišite še enkrat geslo" class="form-control" type="password" name="gesloPonovno" id="gesloPonovno" required>
-				 </div>
-          </div>
-		  </div>
-		  
-					<button align="center" id="button"  type="submit">REGISTRACIJA</button>
-	 </div>	 
-		 		 
-		 
-</div>
+<body>
 
+<article>
+  <header>
+    <h1 style="color:yellow" id="glava" >Registracija</h1>
+  </header>
+</article>
+	<div id="container" class="row">
+	
+		</br>
+		<center>
+		<form class="col-3 rcorners1" class="" id="formRegistracija" method="post" role="form" action="<?php echo $this->config->base_url(); ?>CtrMain/registracija_perform">
+						
+		<table   style="background: #FBEEC1;">
+			
+			<tr class="col-1">
+			<td class="col-1" style="background: #8EE4AF;">
+			<label>Ime</label>
+			</td>
+			<td class="col-1">
+			<input placeholder="Vpišite ime" type="text" class="form-control" name="ime" id="ime"  required/>
+								
+			</td>
+			</tr>
+				<tr class="col-1">
+			<td class="col-1" style="background: #8EE4AF;">
+			<label>Priimek</label>
+			</td>
+			<td class="col-1">
+			<input placeholder="Vpišite priimek" type="text" name="priimek" id="priimek" class="form-control" required>
+			
+			</td>
+			</tr>
+				<tr class="col-1">
+			<td class="col-1" style="background: #8EE4AF;">
+			<label>E-mail</label>
+			</td>
+			<td class="col-1">
+			<input placeholder="Vpišite vaš email račun" class="form-control" type="text" name="email" id="email" required>
+								</td>
+			</tr>
+				<tr class="col-1">
+			<td class="col-1" style="background: #8EE4AF;">
+			<label>Potrdite E-mail</label>
+			</td>
+			<td class="col-1">
+			<input placeholder="Vpišite še enkrat vaš e-mail" type="text"  class="form-control" name="emailPonovno" id="emailPonovno" required>
+			</td>
+			</tr>
+			
+			<tr class="col-1">
+			<td class="col-1" style="background: #8EE4AF;">
+			<label>Geslo</label>
+			</td>
+			<td class="col-1">
+			<input placeholder="Vpišite geslo" type="password" name="geslo" id="geslo" class="form-control" required>
+			
+			</td>
+			</tr>
+			
+			<tr class="col-1">
+			<td class="col-1" style="background: #8EE4AF;">
+			<label>Potrditev Gesla</label>
+			</td>
+			<td class="col-1">
+			<input placeholder="Vpišite še enkrat geslo" class="form-control" type="password" name="gesloPonovno" id="gesloPonovno" required>
+			
+			</td>
+			</tr>
+			
+			
+			<tr class="col-1">
+			<td class="col-1"  colspan="2"   style="background: #FBEEC1;"><a style="color:red" href="<?php echo $this->config->base_url(); ?>CtrMain/login"  class="gumbDodajDogodek" >NAZAJ</a>
+			
+					<button style="color:blue" class="gumbDodajDogodek" align="center" id="button"  type="submit">REGISTRACIJA</button>
+					
+					</td>
+			</tr>
+			</table>
+				
+		</form>
+		</center>
+	</div>
+
+		
 
 		
 		
